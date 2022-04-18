2f7e8896899c52ea8f1ac0f2e2c072fa4c530c8f
yura-poj <yurap6002@gmail.com>
2022-04-18 14:14:06 +0700

    add gem decent exposure

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem "bootsnap", require: false
 
 gem 'slim-rails'
 
+gem 'decent_exposure', '~> 3.0'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,18 @@
 class QuestionsController < ApplicationController
-  before_action :find_questions, only: %i[index ]
-
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
+  expose :questions, ->{ Question.all }
+  expose :question
 
   def create
-    @question = Question.new(question_params)
-    if @question.save
-      redirect_to @question
+    if question.save
+      redirect_to question
     else
       render :new
     end
   end
 
-  def edit; end
-
   def update
-    question.update(question_params)
-    if @question.save
-      redirect_to @question
+    if question.update(question_params)
+      redirect_to question
     else
       render :edit
     end
@@ -32,21 +20,10 @@ class QuestionsController < ApplicationController
 
   def destroy
     question.destroy
-
     redirect_to questions_path
   end
 
   private
-
-  def find_questions
-    @questions = Question.all
-  end
-
-  def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
-  end
-
-  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)

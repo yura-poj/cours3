44727cfe4f1b1391888b02abe5ed21bc0ff9bd73
yura-poj <yurap6002@gmail.com>
2022-05-30 16:38:15 +0700

    add files to answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :author, class_name: 'User'
+  has_many_attached :files
 
   validates :body, presence: true
 
--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -8,13 +8,29 @@ feature 'User can create answer' do
   given!(:question) { create(:question, author: user) }
   given!(:answers) { create_list(:answer, 2, question: question, author: user) }
 
-  scenario 'with errors', js: true do
-    sign_in(user)
+  describe 'User create answer', js: true do
+    scenario 'with errors' do
+      sign_in(user)
 
-    visit question_path(question)
+      visit question_path(question)
+      click_on 'Reply answer'
+      click_on 'Reply'
 
-    click_on 'Reply'
-    expect(page).to have_content "Body can't be blank"
+      expect(page).to have_content "Body can't be blank"
+    end
+
+    scenario 'with attached file' do
+      sign_in(user)
+
+      visit question_path(question)
+      click_on 'Reply answer'
+      fill_in 'Body', with: 'text text text'
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Reply'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
 end

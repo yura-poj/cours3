3e201ba76a792db82f59d3493c14aa1990046547
yura-poj <yurap6002@gmail.com>
2022-07-22 15:53:21 +0700

    fix

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -57,7 +57,7 @@ describe 'Questions API', type: :request do
         end
 
         it 'returns all public fields' do
-          %w[id body author_id created_at updated_at].each do |attr|
+          %w[id body created_at updated_at].each do |attr|
             expect(answer_response[attr]).to eq answer.send(attr).as_json
           end
         end
